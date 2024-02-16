c54cf3f154ac1f4bb23e1394bd0051cb36913077
Juande Pérez <jddperezj1@gmail.com>
2024-02-16 17:42:56 +0100

    cambios en humor

--- Jam17/Assets/Scripts/DnDScript.cs
+++ Jam17/Assets/Scripts/DnDScript.cs
@@ -18,8 +18,8 @@ public class DnDScript : MonoBehaviour
     {
 
         tablaHumor = new int[3, 6]{
-            {3,2,0,3,-2,-3},
-            {-2,-1,-2,2,1,3},
+            {3,1,0,3,-2,-2},
+            {0,-1,-3,2,1,2},
             {-3,1,1,-3,3,-2}};
 
         spriteRenderer = GetComponent<SpriteRenderer>();
